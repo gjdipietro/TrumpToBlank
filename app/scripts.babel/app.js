2e98c98f68cd9fd0a5b7c29d1e8234a36e00ecef
Barb Schramm <Barb Schramm>
2017-02-13 22:41:23 -0500

    Header change to just red, new font, clean up content script logic

--- app/scripts.babel/app.js
+++ app/scripts.babel/app.js
@@ -13,10 +13,8 @@ function init() {
 
 function updateWord() {
 	chrome.runtime.sendMessage({action: 'updateWord', word: blankWord.value	});
-
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {word: blankWord.value});
 	});
-
 	window.close();
 }
--- app/scripts.babel/content_script.js
+++ app/scripts.babel/content_script.js
@@ -1,59 +1,65 @@
+const contentScript = (function(){
+	//globals
+	let blankWord;
 
-function crawlWithVal(val) {
-    walk(document.body);
+	//add listeneres
+	addListeners();
+	
+	//start program
+	init();
 
-	function walk(node) {
-		// I stole this function from here:
-		// http://is.gd/mwZp7E
+	function init() {
+		chrome.storage.sync.get('blankWord', function (val) {
+			blankWord = val.blankWord;
+			crawlWithVal(blankWord);
+		});
+	}
 
-		var child, next;
-		if (node.tagName) {
-			if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea') {
-				return;
+	function addListeners() {
+		chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    		if (request.word) {
+				crawlWithVal(request.word)
+    		}
+		});
+	}
+	
+	function crawlWithVal(val) {
+		walk(document.body);
+
+	   function walk(node) {
+			// I stole this function from here:
+			// http://is.gd/mwZp7E
+			var child, next;
+			if (node.tagName) {
+				if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea') {
+					return;
+				}
+			}
+			
+			switch (node.nodeType)  {
+				case 1:  // Element
+				case 9:  // Document
+				case 11: // Document fragment
+					child = node.firstChild;
+					while (child) {
+						next = child.nextSibling;
+						walk(child);
+						child = next;
+					}
+					break;
+				case 3: // Text node
+					handleText(node);
+					break;
 			}
 		}
-		
-		switch (node.nodeType)  {
-			case 1:  // Element
-			case 9:  // Document
-			case 11: // Document fragment
-				child = node.firstChild;
-				while ( child ) 
-				{
-					next = child.nextSibling;
-					walk(child);
-					child = next;
-				}
-				break;
 
-			case 3: // Text node
-				handleText(node);
-				break;
+		function handleText(textNode) {
+			let v = textNode.nodeValue;
+			if (val === null || val === undefined) {
+				return false;
+			}
+			v = v.replace(/\bTRUMP\b/gi, val); 
+			textNode.nodeValue = v;
 		}
 	}
-
-	function handleText(textNode) {
-		var v = textNode.nodeValue;
-		if (val) {
-			v = v.replace(/\bTrump\b/g, val);
-			v = v.replace(/\bTRUMP\b/g, val);
-		}
-		textNode.nodeValue = v;
-	}
-}
-
-function init(callback) {
-    let blankWord;
-    chrome.storage.sync.get('blankWord', function (val) {
-        blankWord = val.blankWord;
-        callback(blankWord);
-    });
-}
-
-init(crawlWithVal);
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.word) {
-		crawlWithVal(request.word)
-    }
-});
+})();
